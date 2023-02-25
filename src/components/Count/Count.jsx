4da5338c8ead42cd549e774e340a9be2615d160c
Aleksandr Krasavin <krasa19920906@gmail.com>
2023-02-25 11:16:52 +0300

    added states for counter and keys for ul

--- src/components/Count/Count.jsx
+++ src/components/Count/Count.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import style from "./Count.module.css";
 
-export const Count = ({count}) => {
+export const Count = (props) => {
+    let [count,setCount] = useState(props.count);
+  
+    const countDicrement = () => {
+        if (count > 0) {
+            setCount(count - 1)
+        }
+    }
+    const countIncrement = () => {
+        setCount(count + 1)
+    }
     return ( 
         <div className={style.count}>
-            <button className={style.minus}>-</button>
+            <button onClick={countDicrement} className={style.minus}>-</button>
             <p className={style.amount}>{count}</p>
-            <button className={style.plus}>+</button>
+            <button onClick={countIncrement} className={style.plus}>+</button>
         </div>
-    )
+    );
 }
