7ce91d52f210fc3ab1ed7ea924dffdaabb512a94
Aleksandr Krasavin <krasa19920906@gmail.com>
2023-02-25 12:42:49 +0300

    redax connection and functionality for navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,16 +1,32 @@
 import classNames from "classnames";
+import { useDispatch, useSelector } from "react-redux";
+import { changeCategory } from "../../store/category/categorySlice";
 import { Container } from "../Container/Container";
 import style from "./Navigation.module.css";
 
 export const Navigation = () => {
+    const {category,activeCategory} = useSelector(state => state.category);
+    const dispath = useDispatch()
+
     return (
         <nav className={style.navigation}>
             <Container className={style.container}>
                 <ul className={style.list}>
-                    <li className={style.item}>
-                        <button className={classNames(style.button,style.button_burger,style.button_active)}>Бургеры</button>
-                    </li>
-                    <li className={style.item}>
+                    {category.map((item,i) => 
+                        <li className={style.item}>
+                            <button className={classNames(
+                                style.button,
+                                activeCategory === i ? style.button_active : "")}
+                                style = {{backgroundImage:`url(${item.image})`}}
+                                onClick = {() => {
+                                    dispath(changeCategory({indexCategory:i}))
+                                }}
+                            >
+                                {item.rus}
+                            </button>
+                        </li>
+                    )}
+                    {/* <li className={style.item}>
                         <button className={classNames(style.button,style.button_snack)}> Закуски</button>
                     </li>
                     <li className={style.item}>
@@ -33,7 +49,7 @@ export const Navigation = () => {
                     </li>
                     <li className={style.item}>
                         <button className={classNames(style.button,style.button_sauce)}>Соусы</button>
-                    </li>
+                    </li> */}
                 </ul>
             </Container>
         </nav>
